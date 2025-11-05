db40be723a6c52d1cb650ac719a9772784f2b073
TRAVERSINO Eoghan <eoghan.traversino@bordeaux-inp.fr>
2025-11-05 18:12:58 +0100

    Chagement du signal de parole Création d'un signal bruité

--- src/part2_2.m
+++ src/part2_2.m
@@ -3,3 +3,34 @@ close all
 clc
 
 %Projet Filtrage et Estimateurs
+%Chargement d'un signal de parole
+signal1 = load('../data/fcno01fz.mat'); %Attention, c'est un struct
+
+%Paramètres
+sigma_carre = 0.5;
+len_signal1 = length(signal1.fcno01fz)
+
+%Génération d'un bruit blanc gaussien centré
+Bruit = sigma_carre * randn(1,len_signal1);
+
+%Signal bruité
+signalBruit = signal1.fcno01fz + Bruit; % Ajout du bruit au signal %Il y a un transpose a faire 
+
+%Affichage
+
+%Signal de parole
+figure;
+plot(signal1.fcno01fz)
+title('Représentation du 1er signal de parole')
+grid on
+
+%Bruit BBGC
+figure;
+plot(Bruit);
+grid on;
+
+% Signal bruité
+figure;
+plot(signalBruit);
+title('Signal de parole bruité');
+grid on;
