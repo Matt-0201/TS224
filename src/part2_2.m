1d3d70bf26fb57518681c03f26c2240c639cf03f
TRAVERSINO Eoghan <eoghan.traversino@bordeaux-inp.fr>
2025-11-07 12:03:16 +0100

    Mise en place d'un traitement de signal bruitant un signal de parole avec un bruit additif Mise en place de ce bruit avec et sans RSB

--- src/part2_2.m
+++ src/part2_2.m
@@ -3,34 +3,67 @@ close all
 clc
 
 %Projet Filtrage et Estimateurs
+
 %Chargement d'un signal de parole
 signal1 = load('../data/fcno01fz.mat'); %Attention, c'est un struct
 
-%Paramètres
-sigma_carre = 0.5;
-len_signal1 = length(signal1.fcno01fz)
+%Ecoute du signal non bruité
+%soundsc(signal1.fcno01fz);
+
+% ===== Paramètres
+s = signal1.fcno01fz;
+sigma_carre = 100;
+len_signal1 = length(s);
+RSB = 5; % = 5:5:15;
 
 %Génération d'un bruit blanc gaussien centré
-Bruit = sigma_carre * randn(1,len_signal1);
+Bruit = randn(1,len_signal1);
+len_bruit = length(Bruit);
 
-%Signal bruité
-signalBruit = signal1.fcno01fz + Bruit; % Ajout du bruit au signal %Il y a un transpose a faire 
+% ===== Signal bruité sans ajustement ni maitrise du RSB
+signalBruit = s +  sigma_carre *Bruit'; % Ajout du bruit au signal avec transposition
 
-%Affichage
 
+%Détermination de la puissance des signaux
+%Puissance signal1
+Ps= (1/len_signal1)*sum(s.^2);
+fprintf('Ps = %.3f\n',Ps);
+
+%Puissance du bruit
+Pb= (1/len_bruit)*sum(Bruit.^2);
+fprintf('Pb = %.3f\n',Pb);
+
+%Ajustement
+aj = Ps/(Pb * 10^(RSB/10));
+alpha = sqrt(aj);
+
+% ===== Signal bruité avec ajustement du RSB
+signalBruit_ajuste = s + alpha * Bruit'; 
+
+% ===== Affichage
 %Signal de parole
 figure;
-plot(signal1.fcno01fz)
+plot(s)
 title('Représentation du 1er signal de parole')
 grid on
 
 %Bruit BBGC
-figure;
-plot(Bruit);
-grid on;
+% figure;
+% plot(Bruit);
+% grid on;
 
-% Signal bruité
+%Signal bruité sans ajustmement du RSB
 figure;
 plot(signalBruit);
-title('Signal de parole bruité');
+title('Signal de parole bruité sans ajustement du RSB');
 grid on;
+
+%soundsc(signalBruit);
+
+%Signal bruité avec ajustement du RSB
+figure;
+plot(signalBruit_ajuste);
+title(['Signal de parole bruité avec ajustement du RSB (RSB = ' num2str(RSB) ' dB)']);
+grid on;
+
+%soundsc(signalBruit_ajuste);
